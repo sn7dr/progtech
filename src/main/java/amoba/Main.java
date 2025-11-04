4af32854d6ed9dbcbd6c1011518f339da5071d15
snr <sandor.szmolen@gmail.com>
2025-11-04 08:51:56 +0100

    Position class v1 Pom + dependencies

---
+++ src/main/java/amoba/Main.java
@@ -0,0 +1,8 @@
+package amoba;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("mainrun / clean test");
+        System.out.println("mainrun / clean test");
+    }
+}
